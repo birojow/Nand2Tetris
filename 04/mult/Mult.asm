971b6ab2ed024164675109cc71b7f35d64829dfb
Fabiano Augusto de Mello <f.augmello@hotmail.com>
2022-11-20 14:40:08 -0300

    Multiplication

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,41 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@R2
+M=0
+
+@R0
+D=M
+@SET_RESULT_AS_ZERO
+D; JEQ
+@END
+D; JLT
+
+@R1
+D=M
+@SET_RESULT_AS_ZERO
+D; JEQ
+@END
+D; JLT
+
+(PERFORM_MULTIPLICATION)
+@R0
+D=M
+@R2
+M=M+D
+@R1
+M=M-1
+@R1
+D=M
+@END
+D; JEQ
+@PERFORM_MULTIPLICATION
+0; JMP
+
+(SET_RESULT_AS_ZERO)
+@R2
+M=0
+
+(END)
+@END
+0, JMP
