8d467ab4278e18833783a581928d2078bf3a808d
Fabiano Augusto de Mello <f.augmello@hotmail.com>
2022-11-26 16:00:24 -0300

    Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,48 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@SCREEN
+D=A
+@screen_current_address
+M=D
+@8192
+D=D+A
+@screen_end_address
+M=D
+
+(LOOP)
+@screen_current_address
+D=M
+@screen_end_address
+D=D-M
+@NO_OVERFLOW
+D; JLE
+@SCREEN
+D=A
+@screen_current_address
+M=D
+
+(NO_OVERFLOW)
+@KBD
+D=M
+@CLEAR_SCREEN
+D; JEQ
+
+// Fill the screen
+@screen_current_address
+A=M
+M=-1
+@screen_current_address
+M=M+1
+@LOOP
+0; JMP
+
+(CLEAR_SCREEN)
+@screen_current_address
+A=M
+M=0
+@screen_current_address
+M=M+1
+@LOOP
+0; JMP
